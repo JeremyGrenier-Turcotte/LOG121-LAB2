a256a6f110f4f9fd62fe099715bcf1212b7d8398
Jérémy Grenier-Turcotte <jeremy.grenier-turcotte.1@ens.etsmtl.ca>
2018-11-02 14:52:43 -0400

    implémentation TestDe et TestJoueur

--- src/test/TestDe.java
+++ src/test/TestDe.java
@@ -9,15 +9,43 @@ import org.junit.Test;
 public class TestDe {
 
 	private De de1;
+	private De de2;
+	
+	private void setValeur(De de, int valeur){
+		while(de.getValeur() != valeur)
+		{
+		       de.brasser();
+		}
+	}
 	
 	@Before
 	public void faireAvant(){
 		de1 = new De(6);
+		de2 = new De(6);
 	}
 	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void constructeurTest() {
+		int valeurInitDe = 1;
+		assertEquals(valeurInitDe,de1.getValeur());
+	}
+	
+	public void valeurSuperieureTest(){
+		setValeur(de1, 6);
+		setValeur(de2, 1);
+		assertEquals(1, de1.compareTo(de2));
+	}
+	
+	public void valeurInferieureTest(){
+		setValeur(de1, 1);
+		setValeur(de2, 6);
+		assertEquals(-1, de1.compareTo(de2));
+	}
+	
+	public void memeValeurTest(){
+		setValeur(de1, 1);
+		setValeur(de2, 1);
+		assertEquals(0, de1.compareTo(de2));
 	}
 
 }
--- src/test/TestJoueur.java
+++ src/test/TestJoueur.java
@@ -1,14 +1,49 @@
 package test;
 
 import static org.junit.Assert.*;
+import generic.Joueur;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestJoueur {
+	
+	private Joueur joueur1;
+	private Joueur joueur2;
 
+	@Before
+	public void faireAvant(){
+		joueur1 = new Joueur(1);
+		joueur2 = new Joueur(2);
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void constructeurTest() {
+		int idJoueur = 1;
+		int scoreInitJoueur = 0;
+		assertEquals(idJoueur,joueur1.getId());
+		assertEquals(scoreInitJoueur, joueur1.getScore());
+	}
+	
+	@Test
+	public void scoreSuperieurTest(){
+		joueur1.ajouterAuScore(1);
+		joueur2.ajouterAuScore(0);
+		assertEquals(1,joueur1.compareTo(joueur2));
+	}
+	
+	@Test
+	public void scoreInferieurTest(){
+		joueur1.ajouterAuScore(0);
+		joueur2.ajouterAuScore(1);
+		assertEquals(-1,joueur1.compareTo(joueur2));
+	}
+	
+	@Test
+	public void memeScoreTest(){
+		joueur1.ajouterAuScore(1);
+		joueur2.ajouterAuScore(1);
+		assertEquals(0,joueur1.compareTo(joueur2));
 	}
 
 }
