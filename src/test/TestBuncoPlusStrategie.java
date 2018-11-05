b1f06f51e2f59cb4f4a441a24e414c767c238cdf
PhilippeGervais <philippe-gervais.bang-nyamba.1@ens.etsmtl.ca>
2018-11-05 15:27:53 -0500

    TestCalculerVainqueurComplete

--- src/test/TestBuncoPlusStrategie.java
+++ src/test/TestBuncoPlusStrategie.java
@@ -8,6 +8,7 @@ import generic.CollectionJoueurs;
 import generic.Jeu;
 import generic.Joueur;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class TestBuncoPlusStrategie {
@@ -17,27 +18,40 @@ public class TestBuncoPlusStrategie {
     Joueur joueur2;
     Joueur joueur3;
     CollectionJoueurs joueurs;
-
+    
     @Before
-    public void initialize() {
+    public void instanciation() {
         joueur1 = new Joueur(1);
-        joueur1.ajouterAuScore(1);
         joueur2 = new Joueur(2);
-        joueur2.ajouterAuScore(2);
         joueur3 = new Joueur(3);
-        joueur3.ajouterAuScore(3);
         joueurs = new CollectionJoueurs();
         joueurs.ajouterJoueur(joueur1);
         joueurs.ajouterJoueur(joueur2);
         joueurs.ajouterJoueur(joueur3);
-        buncoPlus = new BuncoPlus(null, null, null);
         buncoPlusStrategie = new BuncoPlusStrategie();
+        buncoPlus = new BuncoPlus(null, joueurs, buncoPlusStrategie);    	
     }
 
     @Test
-    public void testCalculerVainqueur() {
+    public void testCalculerVainqueurAvecScoresDifferents() {
+        joueur1.ajouterAuScore(10);
+        joueur2.ajouterAuScore(20);
+        joueur3.ajouterAuScore(30);
         Joueur[] joueurs = buncoPlusStrategie.calculerLeVainqueur(buncoPlus);
         assertSame(joueur3,joueurs[0]);
+        assertSame(joueur2,joueurs[1]);
+        assertSame(joueur1,joueurs[2]);
+    }
+
+    @Test
+    public void testCalculerVainqueurAvecScoresIdentiques() {
+        joueur1.ajouterAuScore(10);
+        joueur2.ajouterAuScore(10);
+        joueur3.ajouterAuScore(10);
+        Joueur[] joueurs = buncoPlusStrategie.calculerLeVainqueur(buncoPlus);
+        assertSame(joueur1,joueurs[0]);
+        assertSame(joueur2,joueurs[1]);
+        assertSame(joueur3,joueurs[2]);
     }
 
 }
