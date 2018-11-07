ae30ba6bb566c7f363a7a559505e199905157eef
PhilippeGervais <philippe-gervais.bang-nyamba.1@ens.etsmtl.ca>
2018-11-07 16:07:21 -0500

    Correction de la méthode jeu.calculerScoreTour()

--- src/bunco/BuncoPlus.java
+++ src/bunco/BuncoPlus.java
@@ -8,32 +8,7 @@ import generic.Joueur;
 
 public class BuncoPlus extends Jeu{
 
-	private int tourCourant = 1;
-
 	public BuncoPlus(CollectionDes collDes, CollectionJoueurs collJoueurs, IStrategie strat) {
 		super(collDes, collJoueurs, strat);
 	}
-
-	public int getTourCourant() {
-	    return tourCourant;
-    }
-
-	@Override
-    public void effectuerTour() {
-	    super.effectuerTour();
-	    tourCourant++;
-    }
-
-    @Override
-    public void calculerScoreTour() {
-		int score = strat.calculerScoreTour(this);
-        joueurCourant.ajouterAuScore(score);
-
-        // Si le joueur n'a pas fait de points ou qu'il a fait un bunco, on passe au joueur suivant.
-        if(score == 0 || score == 21) {
-            peutPasserAuSuivant = true;
-        } else {
-            peutPasserAuSuivant = false;
-        }
-    }
 }
