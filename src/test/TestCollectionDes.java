6a24df0f0943ec43928b102a30264af67e013332
Antonin Courtin <antonin.courtin.1@ens.etsmtl.ca>
2018-11-09 17:07:15 -0500

    Test collections

--- src/test/TestCollectionDes.java
+++ src/test/TestCollectionDes.java
@@ -1,15 +1,12 @@
 package test;
 
-import static org.junit.Assert.*;
+import generic.CollectionDes;
+import generic.De;
+import org.junit.Test;
 
 import java.util.Iterator;
 
-import generic.CollectionDes;
-import generic.De;
-import generic.IterateurDes;
-
-import org.junit.Before;
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class TestCollectionDes extends TestBase{
 	
@@ -17,14 +14,42 @@ public class TestCollectionDes extends TestBase{
 	private Iterator<De> itrDes1;
 	private Iterator<De> itrDes2;
 
+
+
 	@Test
 	public void testAjouterDe() {
+		collDes2 = new CollectionDes();
+		De d = new De(6);
+		collDes2.ajouterDe(new De(6));
+		collDes2.ajouterDe(d);
+		collDes2.ajouterDe(new De(6));
+		itrDes1=collDes2.iterator();
+		int index=0;
+		while(itrDes1.hasNext()){
+			De d2=itrDes1.next();
+			if(index==1){
+				assertEquals(d2, d);
+			}
+			index++;
+		}
+
+	}
+
+	@Test
+	public void testEquals(){
 		collDes2 = new CollectionDes();
 		collDes2.ajouterDe(new De(6));
 		collDes2.ajouterDe(new De(6));
 		collDes2.ajouterDe(new De(6));
-		
-		assertEquals(collectionDes, collDes2);
+
+		assertEquals(collDes2.equals(collectionDes),true);
+
+	}
+	@Test
+	public void getNbDes(){
+
+		assertEquals(collectionDes.getNbDes(),3);
+
 	}
 
 }
--- src/test/TestCollectionJoueurs.java
+++ src/test/TestCollectionJoueurs.java
@@ -1,13 +1,12 @@
 package test;
 
-import static org.junit.Assert.*;
+import generic.CollectionJoueurs;
+import generic.Joueur;
+import org.junit.Test;
 
 import java.util.Iterator;
 
-import generic.CollectionJoueurs;
-import generic.Joueur;
-
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class TestCollectionJoueurs extends TestBase{
 	
@@ -18,11 +17,37 @@ public class TestCollectionJoueurs extends TestBase{
 	@Test
 	public void testAjouterJoueur() {
 		collJoueurs2 = new CollectionJoueurs();
+		Joueur j = new Joueur(2);
+		collJoueurs2.ajouterJoueur(new Joueur(1));
+		collJoueurs2.ajouterJoueur(j);
+		collJoueurs2.ajouterJoueur(new Joueur(3));
+		itrJoueur1=collJoueurs2.iterator();
+		int index=0;
+		while(itrJoueur1.hasNext()){
+			Joueur j2=itrJoueur1.next();
+			if(index==1){
+				assertEquals(j2, j);
+			}
+			index++;
+		}
+
+	}
+
+	@Test
+	public void testEquals(){
+		collJoueurs2 = new CollectionJoueurs();
 		collJoueurs2.ajouterJoueur(new Joueur(1));
 		collJoueurs2.ajouterJoueur(new Joueur(2));
 		collJoueurs2.ajouterJoueur(new Joueur(3));
-		
-		assertEquals(collJoueurs, collJoueurs2);
+
+		assertEquals(collJoueurs2.equals(collJoueurs),true);
+
+	}
+	@Test
+	public void getNbJoueur(){
+
+		assertEquals(collJoueurs.getNbJoueurs(),3);
+
 	}
 
 }
